4e5b6c59ebf591f272435d4cea7ab9dde5ef4226
Arun Chauhan <arunchauhan@Aruns-MacBook-Pro.local>
2026-07-18 15:30:14 +0530

    chore: update release android app bundle with R8 minification, resource shrinking, and obfuscation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,6 +55,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
