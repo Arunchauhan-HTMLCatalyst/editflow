6605d9d704800c310e844f62715b01ce794608b2
Arun Chauhan <arunchauhan@Aruns-MacBook-Pro.local>
2026-07-18 14:29:29 +0530

    deploy: Update web app with threaded replies, reactions & tri-state task status

--- docs/app/flutter_service_worker.js
+++ docs/app/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2d0676bb1ac0c03db878715b2f16ee6d",
+const RESOURCES = {"flutter_bootstrap.js": "22cb8a39260f4ce2a8982768ca64fdc1",
 "version.json": "485593b7caced6076ab2019361c6d119",
 "index.html": "613916f44fd21e315c3217dd7022e7c4",
 "/": "613916f44fd21e315c3217dd7022e7c4",
 "firebase-messaging-sw.js": "1ce78c90f14da03916903507ef7fb107",
-"main.dart.js": "03343790a28eee8bf9e0e7be75a0b21f",
+"main.dart.js": "eff78d979ba36a310af6b615b6cfae87",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "9896dcd3effa3aa778566c353529d5bc",
 "icons/Icon-192.png": "9896dcd3effa3aa778566c353529d5bc",
@@ -26,7 +26,7 @@ const RESOURCES = {"flutter_bootstrap.js": "2d0676bb1ac0c03db878715b2f16ee6d",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "4a9b0b0f0d78566fede99f3bad88e693",
-"assets/fonts/MaterialIcons-Regular.otf": "a7c4fc89b22cd0ccce431f3e5817e904",
+"assets/fonts/MaterialIcons-Regular.otf": "7b90eb09da32f620894bd9ce5f73bac9",
 "assets/assets/images/whatsapp_logo.png": "774352deeaa60ebe04164a1e28489771",
 "assets/assets/images/upi_logo.png": "02e0e2a83c0a7fbe88382f7abc2c90d6",
 "assets/assets/images/google_logo.png": "579d58ce3b333812d6c65f7126044383",
